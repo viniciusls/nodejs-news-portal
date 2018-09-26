a32897a4454c0a646d9c1e55892ce0f4868a4241
Vinicius Silva <vinicius768@msn.com>
2018-09-26 12:21:47 -0300

    Fixed server port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require('./config/server');
 const routes = require('./app/routes/web')(app);
 
-app.listen(300, () => {
+app.listen(3000, () => {
     console.log('Server is running');
 });
