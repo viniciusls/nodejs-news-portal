2ea54e2a327aa5a3d0c61b7a9e1226588690c257
Vinicius Silva <vinicius768@msn.com>
2018-09-26 12:44:31 -0300

    Refactoring on routes

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,15 +1,31 @@
 module.exports = (application) => {
     application.post('/news/create', (req, res) => {
-        const noticia = req.body;
+        const news = req.body;
+
+        req.assert('title', 'Título não pode ser vazio.').notEmpty();
+        req.assert('author', 'Autor não pode ser vazio.').notEmpty();
+        req.assert('content', 'Notícia não pode ser vazia.').notEmpty();
+        req.assert('summary', 'Resumo não pode ser vazio.').notEmpty();
+        req.assert('summary', 'Resumo deve conter entre 10 e 100 caracters.').len(10, 100);
+        req.assert('news_at', 'Data da notícia não pode ser vazia.').notEmpty().isDate({ format: 'YYYY-MM-DD' });
+
+        const errors = req.validationErrors();
+
+        if (errors) {
+            res.redirect('/news/create', { errors, news });
+
+            return;
+        }
+
         const connection = application.config.database();
         const newsModel = new application.app.models.NoticiasDAO(connection);
 
-        newsModel.save(noticia, (error, result) => {
+        newsModel.save(news, (error, result) => {
             res.redirect('news');
         });
     });
 
-    application.applicationget('/news/create', (req, res) => {
+    application.get('/news/create', (req, res) => {
         res.render('admin/news/create');
     });
 };
