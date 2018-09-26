df632e0a2788e0cb0ffd40c98a8900dfcacd48d2
Vinicius Silva <vinicius768@msn.com>
2018-09-26 12:18:27 -0300

    Implementation of news add

--- app/routes/web.js
+++ app/routes/web.js
@@ -1,6 +1,15 @@
 const database = require('../../config/database');
 
 module.exports = (app) => {
+    app.post('/news/create', (req, res) => {
+        const noticia = req.body;
+        const connection = database();
+
+        connection.query('INSERT INTO news SET ?', noticia, (error, result) => {
+            res.redirect('news/index');
+        });
+    });
+
     app.get('/news/create', (req, res) => {
         res.render('admin/news/create');
     });
