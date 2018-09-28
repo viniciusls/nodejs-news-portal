a28efbadd48c7bc5b8fa6f87ccca736d4e3b9df9
Vinicius Silva <vinicius768@msn.com>
2018-09-28 15:08:20 -0300

    Fixed some errors

--- app/models/NewsDAO.js
+++ app/models/NewsDAO.js
@@ -2,15 +2,15 @@ function NewsDAO(connection) {
     this.connection = connection;
 }
 
-NewsDAO.prototype.save = function (callback, news) {
+NewsDAO.prototype.save = function (news, callback) {
     this.connection.query('INSERT INTO news SET ?', news, callback);
 };
 
 NewsDAO.prototype.getAll = function (callback) {
-    this.connection.query('SELECT * FROM noticias', callback);
+    this.connection.query('SELECT * FROM news', callback);
 };
 
-NewsDAO.prototype.getById = function (callback, id) {
+NewsDAO.prototype.getById = function (id, callback) {
     this.connection.query('SELECT * FROM news WHERE ID = ?', id, callback);
 };
 
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,9 +1,9 @@
 module.exports = (application) => {
-    application.post('/news/create', (req, res) => {
+    application.get('/news/create', (req, res) => {
         application.app.controllers.admin.create(application, req, res);
     });
 
-    application.get('/news/create', (req, res) => {
-        application.app.controllers.admin.save(application, req, res);
+    application.post('/news/create', (req, res) => {
+        application.app.controllers.admin.store(application, req, res);
     });
 };
