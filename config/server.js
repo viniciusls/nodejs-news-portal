d9ec524295f7d40e17190b102ffee68f7910ef1c
Vinícius Silva <vinicius768@msn.com>
2018-09-30 10:08:58 -0300

    Added static content

--- config/server.js
+++ config/server.js
@@ -1,11 +1,14 @@
-const app = require('express')();
+const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 
+const app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 
