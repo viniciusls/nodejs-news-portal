e1ee9d6d50854a6de27057892bd987697d8f1f6d
Vinícius Silva <vinicius768@msn.com>
2018-09-30 10:37:05 -0300

    Implemented home view logic

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,7 +2,7 @@ module.exports.index = function (application, req, res) {
     const connection = application.config.database();
     const newsModel = new application.app.models.NewsDAO(connection);
 
-    newsModel.getAll(50, 0, (error, result) => {
+    newsModel.getAll(5, 0, (error, result) => {
         res.render('home/index', { news: result });
     });
 };
